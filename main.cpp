631869a582c97b1fce706c3e09d7d94166821ad8
Jijovo <jijovo77@gmail.com>
2026-03-01 12:02:43 -0800

    final

--- main.cpp
+++ main.cpp
@@ -35,9 +35,35 @@ int main() {
     }
     //define vector container
     vector<Movie> movies;
+    const int size = 6;
+    //read data from file and store in vector
+    string title;
+    int year;
+    string writer;
+    for (int i = 0; i < size; i++) { //videogame reference, not a movie reference, but iykyk
+        getline(fin, title);
+        fin >> year;
+        fin.ignore();
+        getline(fin, writer);
+        Movie m;
+        m.setTitle(title);
+        m.setYear(year);
+        m.setWriter(writer);
+        movies.push_back(m);
+    }
+    fin.close();
+    //test getters
+    for (int i = 0; i < size; i++) {
+        cout << movies[i].getTitle() << " " << movies[i].getYear() << " " << movies[i].getWriter() << endl;
+    }
+    //test print function
+    cout << setw(w) << "Title" << setw(w) << "Year" << setw(w) << "Writer" << endl;
+    for (int i = 0; i < size; i++) {
+        movies[i].print();
+    }
     return 0;
 }
-
+//define print function
 void Movie::print() {
     cout << setw(w) << title << setw(w) << year << setw(w) << writer << endl;
 }
